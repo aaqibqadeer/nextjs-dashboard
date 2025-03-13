c95264b2e38b73e3fbe75453e64a3ced2d1bc38b
aaqibqadeer <aaqibqs@gmail.com>
2025-03-13 01:52:42 -0400

    fix: remove console statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,12 @@ import '@/app/ui/global.css'
 import { inter } from './ui/fonts'
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang='en'>
-      {console.log({ inter })}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   )
